72924e4b18881ca0a28d1aa8b8015936d76e14cd
levis <levismunene01@gmail.com>
2024-06-02 23:58:09 +0300

    done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,61 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        self.__class__.all.append(self)
+
+    @classmethod
+    def all(cls):
+        return cls.all
+
 class Author:
+    all = []
+
     def __init__(self, name):
         self.name = name
+        self.__class__.all.append(self)
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        return list(set([magazine.category for magazine in self.magazines()]))
+
+    @classmethod
+    def all(cls):
+        return cls.all
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self.__class__.all.append(self)
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        authors = [author for author in self.contributors() if len(author.articles()) > 2]
+        return authors if authors else None
+
+    @classmethod
+    def all(cls):
+        return cls.all
